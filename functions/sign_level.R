5d8fb557888e72e06de69b3c0afd4a563bb7e599
Oleh Prylutskyi <90190915+olehprylutskyi@users.noreply.github.com>
2024-04-01 20:57:03 +0300

    test multicollinearity and fit simple models

---
+++ functions/sign_level.R
@@ -0,0 +1,17 @@
+# Custom function to calculate significance level (p-values) for correlation matrix
+# mat : is a matrix of data
+# ... : further arguments to pass to the native R cor.test function
+sign_level <- function(mat, ...) {
+    mat <- as.matrix(mat)
+    n <- ncol(mat)
+    p.mat<- matrix(NA, n, n)
+    diag(p.mat) <- 0
+    for (i in 1:(n - 1)) {
+        for (j in (i + 1):n) {
+            tmp <- cor.test(mat[, i], mat[, j], ...)
+            p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+        }
+    }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
